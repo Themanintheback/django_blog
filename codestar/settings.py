dc292976f56081b7dd22e997463ea093cd84d59f
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-08-31 10:39:28 +0000

    trying

--- codestar/settings.py
+++ codestar/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
- 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
